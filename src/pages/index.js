1fd92687d43126b9bae415a60c60bb8971e2bd98
Aditi Singh <aditisinghdeveloper@gmail.com>
2021-08-22 22:56:23 +0530

    Banner completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 import Banner from "../components/Banner";
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
