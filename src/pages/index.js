30638c4622a06843a19593108411efd63f3facfe
Aditi Singh <aditisinghdeveloper@gmail.com>
2021-08-23 22:50:22 +0530

    Product feed Completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
-export default function Home() {
+import ProductFeed from "../components/ProductFeed";
+
+export default function Home({products}) {
   return (
     <div className="bg-gray-100">
       <Head>
@@ -11,8 +13,19 @@ export default function Home() {
       </Header>
       <main className="max-w-screen-2xl mx-auto">
       <Banner />
-      {/*Product feed*/}
+     <ProductFeed products={products} />
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+      const products = await fetch("https://fakestoreapi.com/products").then(
+        (res) => res.json()
+      );
+       return {
+         props: {
+           products,
+         },
+       };
 }
