8058ea83c809ae055ca95ff580313ca2d879ab2f
Aditi Singh <aditisinghdeveloper@gmail.com>
2021-08-23 23:16:50 +0530

    Product feed slicing

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,7 +3,23 @@ import Product from "./Product";
 function ProductFeed({products}) {
     return (
         <div className="grid grid-flow-row-dense md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
-            {products.map(({id, title, price, description, category, image}) => (
+            {products.slice(0,4).map(({id, title, price, description, category, image}) => (
+             <Product 
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+             />
+                ) )}
+
+        <img className="md:col-span-full" src="https://links.papareact.com/dyz" alt="" />
+
+
+        <div className="md:col-span-2">
+        {products.slice(4,5).map(({id, title, price, description, category, image}) => (
              <Product 
               key={id}
               id={id}
@@ -15,7 +31,23 @@ function ProductFeed({products}) {
              />
                 ) )}
         </div>
-    )
+        
+        {products.slice(5,products.length).map(({id, title, price, description, category, image}) => (
+             <Product 
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+             />
+                ) )}
+
+        </div>
+       
+    
+    );
 }
 
 export default ProductFeed
